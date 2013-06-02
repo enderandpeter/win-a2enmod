30506d2fb350765c128efb0c52b0561be4a8a3e6
Spencer Williams <spencer@aninternetpresence.net>
2013-06-01 22:45:00 -0600

    Line endings. Text recognition. Include .git files.
    The GitHub program suggests these .gitattribute instructions will help
    it to recognize the ps1 files as text rather than binary files, as well
    as insure line ending normalization. I thought I'd explicitly include
    the .git files with the repository just in case the need to be changed
    again. I removed the instruction to create .gitignore from setup.bat.

--- setup.bat
+++ setup.bat
@@ -3,13 +3,6 @@ mklink a2dismod.ps1 a2enmod.ps1
 mklink a2ensite.ps1 a2enmod.ps1
 mklink a2dissite.ps1 a2enmod.ps1
 
-(
-  Echo .*
-  Echo a2dismod.ps1
-  Echo a2ensite.ps1
-  Echo a2dissite.ps1
-) > .gitignore
-
 if %ERRORLEVEL% gtr 0 (
 	echo An error occurred
 ) else (
